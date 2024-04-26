3161661dcd0fef45aed413ea895ec22f97605b3a
Abdullrahmen Ahmed Seif <abdullrahmen2003@gmail.com>
2024-04-26 21:04:21 +0300

    Update common_imports.py

--- common_imports.py
+++ common_imports.py
@@ -1,6 +1,4 @@
 import numpy as np # linear algebra
-import matplotlib.pyplot as plt # data visualization
-import keras # ML platform
+from tensorflow import keras # ML platform
 import os # operating system
 import joblib # save and load preprocessing scalers
-import datetime
